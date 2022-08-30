610a03fda75577e7cce6f0f1b915fb5bda3e241a
Daria Dmitricheva <110775372+12data12@users.noreply.github.com>
2022-08-30 11:28:43 +0300

    Homework1_2

---
+++ Homework1_2/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine ("Enter three numbers in separate lines:");
+
+int numberA = Int32.Parse(Console.ReadLine());
+int numberB = Int32.Parse(Console.ReadLine());
+int numberC = Int32.Parse(Console.ReadLine());
+ 
+int maximal = Math.Max(Math.Max(numberA, numberB), numberC);
+int minimal = Math.Min(Math.Min(numberA, numberB), numberC);
+ 
+Console.WriteLine("{0} < {1} < {2}",
+minimal,
+numberA + numberB + numberC - maximal - minimal,
+maximal);
