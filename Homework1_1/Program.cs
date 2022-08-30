8f7da2430df1691fcfa0da4229a8ffb7142e6608
Daria Dmitricheva <110775372+12data12@users.noreply.github.com>
2022-08-30 11:30:24 +0300

    Homework1_1_correction

--- Homework1_1/Program.cs
+++ Homework1_1/Program.cs
@@ -5,24 +5,23 @@ int numberA, numberB;
 Console.WriteLine ("Enter two numbers in separate lines:");
 
  numberA = Convert.ToInt32(Console.ReadLine());
+ numberB = Convert.ToInt32(Console.ReadLine());
  
-        numberB = Convert.ToInt32(Console.ReadLine());
+    if (numberA > numberB)
+    {
  
-        if (numberA > numberB)
-        {
+        Console.WriteLine(numberA + " is larger than " + numberB);
  
-            Console.WriteLine(numberA + " is larger than " + numberB);
+    }
  
-        }
+    else
  
-        else
- 
-        {
-            if (numberA < numberB)
-            Console.WriteLine(numberA + " is smaller than " + numberB);
-            else 
-            Console.WriteLine(numberA + " is equal to " + numberB);
-        }
+    {
+        if (numberA < numberB)
+        Console.WriteLine(numberA + " is smaller than " + numberB);
+        else 
+        Console.WriteLine(numberA + " is equal to " + numberB);
+    }
   
      Console.ReadLine();
  
