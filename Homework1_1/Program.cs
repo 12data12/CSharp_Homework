e8a4b047ed120f0eb522ebd3b29491e78879be95
Daria Dmitricheva <110775372+12data12@users.noreply.github.com>
2022-08-30 11:00:18 +0300

    Homework1_1_initial

---
+++ Homework1_1/Program.cs
@@ -0,0 +1,28 @@
+﻿{
+int numberA, numberB;
+
+Console.WriteLine ("Enter two numbers in separate lines:");
+
+ numberA = Convert.ToInt32(Console.ReadLine());
+ 
+        numberB = Convert.ToInt32(Console.ReadLine());
+ 
+        if (numberA > numberB)
+        {
+ 
+            Console.WriteLine(numberA + " is larger than " + numberB);
+ 
+        }
+ 
+        else
+ 
+        {
+            if (numberA < numberB)
+            Console.WriteLine(numberA + " is smaller than " + numberB);
+            else 
+            Console.WriteLine(numberA + " is equal to " + numberB);
+        }
+  
+     Console.ReadLine();
+ 
+    }
